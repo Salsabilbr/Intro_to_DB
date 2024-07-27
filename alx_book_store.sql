0b75750dcb27d9c1a7fbd6a0f75a87eae3531e4d
Salsabilbr <sabilbr90@gmail.com>
2024-07-27 04:47:35 -0700

    alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -38,7 +38,8 @@ CREATE TABLE Order_Details (
   orderdetailid INT AUTO_INCREMENT,  
   order_id INT,  
   book_id INT,  
-  quantity INT,  
+  quantity INT,
+  quantity DOUBLE,
   PRIMARY KEY (orderdetailid),  
   FOREIGN KEY (order_id) REFERENCES Orders(order_id),  
   FOREIGN KEY (book_id) REFERENCES Books(book_id)  
